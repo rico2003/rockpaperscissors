08e1b506e97aa6e5f8bf317b7cd6464d5e36fc0f
rico2003 <rickeyswari2003@gmail.com>
2019-11-18 23:06:39 +0000

    I finish two promblem only

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+
 //GLOBAL VARIABLES
 /* global $ */
 $("#shoot").click(function(){
+    let shoot = Math.random();
+    console.log("shoot");
+    let computerOption = "rock";
+    $("#computerChoice").append(computerOption);
 
-)};
+});
+
 
 
 // DOCUMENT READY FUNCTION BELOW
